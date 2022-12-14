71f5bc8bf0c5c7d8965c78ae4e2f260975762be8
Hyuga0615 <115648802+Hyuga0615@users.noreply.github.com>
2022-12-15 00:38:39 +0900

    Revert "03"

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
-    }
-    
-    public function show(Post $post)
-    {
-        return view('posts/show')->with(['post' => $post]);
+        return $post->get();//$postの中身を戻り値にする。
     }
 }
