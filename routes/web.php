ba79f5468000eee0bb70a8212d0b3e908589ac5d
Hyuga0615 <hyuga5884@gmail.com>
2022-12-22 10:25:52 +0900

    8-4

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,8 @@ use App\Http\Controllers\PostController;
 |
 */
 
-<<<<<<< HEAD
 Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
-
+Route::post('/posts', [PostController::class, 'store']);
 ?>
-=======
-Route::get('/', [PostController::class, 'index']);
-
-
-Route::get('/posts/{post}', [PostController::class, 'show']); 
-
->>>>>>> master
